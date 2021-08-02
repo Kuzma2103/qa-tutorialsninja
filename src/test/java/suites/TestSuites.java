fc20d9297717d772f151a244858879a5ede3ddb5
Kuzma2103 <ivankuzmanovic87@gmail.com>
2021-08-02 20:02:15 +0200

    TestSuites added.

---
+++ src/test/java/suites/TestSuites.java
@@ -0,0 +1,24 @@
+package suites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import tests.*;
+import tests_with_login.CheckoutWithLoginTest;
+import tests_with_login.LogoutTest;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+        LoginWithEmptyEmailTest.class,
+        LoginWithEmptyPasswordTest.class,
+        LoginWithWrongUserDataTest.class,
+        CheckoutWithRegisterAccountTest.class,
+        RegistrationTest.class,
+        ValidLoginTest.class,
+        LogoutTest.class,
+        CheckoutWithLoginTest.class,
+        CheckoutAsGuestTest.class,
+})
+
+public class TestSuites {
+}
